379d74218dca9489621b49cbeec155c544edd1c4
Larry Gadea <trivex@gmail.com>
2018-09-09 01:24:11 -0700

    split the javascripts into their own files

---
+++ award-man-grid.js
@@ -0,0 +1,91 @@
+const PAGINATION_SIZE = 100
+
+export default class AwardManGrid {
+  constructor(onRowClicked) {
+    this.grid = null
+    this.onRowClicked = onRowClicked
+  }
+
+  static milesAndCashFormatter(params) {
+    const cost = params.data.costs[params.colDef.headerName.toLowerCase()]
+    if (!cost.miles)
+      return ""
+
+    const cashFormatter = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2})
+    const milesFormatter = new Intl.NumberFormat("en-US")
+    return `${milesFormatter.format(cost.miles / 1000)}k + ${cashFormatter.format(cost.cash)}`
+  }
+
+  static timeOnlyFormatter(params) {
+    const formatter = new Intl.DateTimeFormat("en-US", {month: "numeric", day: "numeric", hour: "numeric", minute: "numeric"})
+    return formatter.format(new Date(params.value))
+  }
+
+  static milesAndCashStyler(params) {
+    if (params.value)
+      return {backgroundColor: "#D5F5E3"}
+    return null
+  }
+
+  // Sort empty/null values as infinite
+  static milesComparator(valueA, valueB) {
+    if (valueA === null && valueB === null)
+      return 0
+    if (valueA === null)
+      return 1
+    if (valueB === null)
+      return -1
+    return valueA - valueB
+  }
+
+  static updateFilterValue(params) {
+    let totalFilterString = ""
+    const model = params.api.getFilterModel()
+    for (const fieldName of Object.getOwnPropertyNames(model)) {
+      const clauseToString = (name, clause) => `${name} ${clause.type} "${clause.filter}"`
+
+      // There are multiple filters
+      if (model[fieldName].condition1) {
+        totalFilterString += `, (${clauseToString(fieldName, model[fieldName].condition1)} ${model[fieldName].operator} ${clauseToString(fieldName, model[fieldName].condition2)})`
+      } else {
+        totalFilterString += `, (${clauseToString(fieldName, model[fieldName])})`
+      }
+    }
+
+    if (totalFilterString)
+      totalFilterString = totalFilterString.substring(2)
+
+    const mainDiv = params.api.gridOptionsWrapper.environment.eGridDiv.querySelector("span[ref=eSummaryPanel]")
+    mainDiv.firstChild.textContent = totalFilterString ? `${totalFilterString} -- ` : ""
+  }
+
+  configureGrid(gridDiv) {
+    const gridOptions = {
+      columnDefs: [
+        {headerName: "Origin Time", field: "fromDateTime", valueFormatter: this.timeOnlyFormatter, width: 110},
+        {headerName: "Dest Time", field: "toDateTime", valueFormatter: this.timeOnlyFormatter, width: 110},
+        {headerName: "Airports", field: "airports", valueGetter: params => `${params.data.fromAirport} -> ${params.data.toAirport}`, width: 100},
+        {headerName: "Flights", field: "flights"},
+        {headerName: "Economy", field: "costs.economy.miles", valueFormatter: this.milesAndCashFormatter, cellStyle: this.milesAndCashStyler, comparator: this.milesComparator, filter: "agNumberColumnFilter", width: 110},
+        {headerName: "Business", field: "costs.business.miles", valueFormatter: this.milesAndCashFormatter, cellStyle: this.milesAndCashStyler, comparator: this.milesComparator, filter: "agNumberColumnFilter", width: 110},
+        {headerName: "First", field: "costs.first.miles", valueFormatter: this.milesAndCashFormatter, cellStyle: this.milesAndCashStyler, comparator: this.milesComparator, filter: "agNumberColumnFilter", width: 110}
+      ],
+      enableSorting: true,
+      enableFilter: true,
+
+      rowSelection: "single",
+      suppressCellSelection: false,
+      suppressRowClickSelection: true,
+
+      pagination: true,
+      paginationPageSize: PAGINATION_SIZE,
+
+      onFilterChanged: this.updateFilterValue,
+      onRowClicked: this.onRowClicked
+    }
+
+    new window.agGrid.Grid(gridDiv, gridOptions)   // eslint-disable-line no-new
+    this.grid = gridOptions
+    this.grid.api.setRowData([])
+  }
+}
--- award-man.js
+++ award-man.js
@@ -1,15 +1,36 @@
 import ApifyRunner from "./apify-runner.js"
-
-const PAGINATION_SIZE = 100
+import AwardManGrid from "./award-man-grid.js"
 
 export default class AwardMan {
-  constructor(config) {
-    this.config = config
+  constructor() {
+    this.config = AwardMan.loadConfigAndUpdateDocument()
 
     this.apify = null
     this.united = null
 
-    this.grid = null
+    this.grid = new AwardManGrid(AwardMan.onRowClicked)
+    this.grid.configureGrid(document.querySelector("#resultsGrid"))
+  }
+
+  static loadConfigAndUpdateDocument() {
+    const config = {
+      apifyToken: localStorage.apifyToken || "",
+      proxyUrl: localStorage.proxyUrl || "",
+      aeroplanUsername: localStorage.aeroplanUsername || "",
+      aeroplanPassword: localStorage.aeroplanPassword || "",
+      origin: localStorage.origin || "",
+      destination: localStorage.destination || "",
+      date: localStorage.date || ""
+    }
+
+    for (const configToSave of Object.getOwnPropertyNames(config)) {
+      const element = document.getElementById(configToSave)
+      element.value = config[configToSave]
+      element.addEventListener("change", () => (config[element.id] = element.value))
+      element.addEventListener("change", () => localStorage.setItem(element.id, element.value))
+    }
+
+    return config
   }
 
   async prep() {
@@ -19,7 +40,7 @@ export default class AwardMan {
   }
 
   async test() {
-    this.grid.api.showLoadingOverlay()
+    this.grid.grid.api.showLoadingOverlay()
     const raw = await this.apify.runActor(this.united, {
       proxyUrl: this.config.proxyUrl,
       from: this.config.origin,
@@ -27,93 +48,14 @@ export default class AwardMan {
       date: this.config.date,
       maxConnections: 2
     })
-    this.grid.api.hideOverlay()
+    this.grid.grid.api.hideOverlay()
 
-    this.grid.api.setRowData(raw.results)
+    this.grid.grid.api.setRowData(raw.results)
 
     console.log("Done.")
   }
 
-  configureGrid(gridDiv) {
-    const milesAndCashFormatter = params => {
-      const cost = params.data.costs[params.colDef.headerName.toLowerCase()]
-      if (!cost.miles)
-        return ""
-
-      const cashFormatter = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2})
-      const milesFormatter = new Intl.NumberFormat("en-US")
-      return `${milesFormatter.format(cost.miles / 1000)}k + ${cashFormatter.format(cost.cash)}`
-    }
-
-    const timeOnlyFormatter = params => {
-      const formatter = new Intl.DateTimeFormat("en-US", {month: "numeric", day: "numeric", hour: "numeric", minute: "numeric"})
-      return formatter.format(new Date(params.value))
-    }
-
-    const milesAndCashStyler = params => {
-      if (params.value)
-        return {backgroundColor: "#D5F5E3"}
-      return null
-    }
-
-    // Sort empty/null values as infinite
-    const milesComparator = (valueA, valueB) => {
-      if (valueA === null && valueB === null)
-        return 0
-      if (valueA === null)
-        return 1
-      if (valueB === null)
-        return -1
-      return valueA - valueB
-    }
-
-    const updateFilterValue = params => {
-      let totalFilterString = ""
-      const model = params.api.getFilterModel()
-      for (const fieldName of Object.getOwnPropertyNames(model)) {
-        const clauseToString = (name, clause) => `${name} ${clause.type} "${clause.filter}"`
-
-        // There are multiple filters
-        if (model[fieldName].condition1) {
-          totalFilterString += `, (${clauseToString(fieldName, model[fieldName].condition1)} ${model[fieldName].operator} ${clauseToString(fieldName, model[fieldName].condition2)})`
-        } else {
-          totalFilterString += `, (${clauseToString(fieldName, model[fieldName])})`
-        }
-      }
-
-      if (totalFilterString)
-        totalFilterString = totalFilterString.substring(2)
-
-      const mainDiv = params.api.gridOptionsWrapper.environment.eGridDiv.querySelector("span[ref=eSummaryPanel]")
-      mainDiv.firstChild.textContent = totalFilterString ? `${totalFilterString} -- ` : ""
-    }
-
-    const gridOptions = {
-      columnDefs: [
-        {headerName: "Origin Time", field: "fromDateTime", valueFormatter: timeOnlyFormatter, width: 110},
-        {headerName: "Dest Time", field: "toDateTime", valueFormatter: timeOnlyFormatter, width: 110},
-        {headerName: "Airports", field: "airports", valueGetter: params => `${params.data.fromAirport} -> ${params.data.toAirport}`, width: 100},
-        {headerName: "Flights", field: "flights"},
-        {headerName: "Economy", field: "costs.economy.miles", valueFormatter: milesAndCashFormatter, cellStyle: milesAndCashStyler, comparator: milesComparator, filter: "agNumberColumnFilter", width: 110},
-        {headerName: "Business", field: "costs.business.miles", valueFormatter: milesAndCashFormatter, cellStyle: milesAndCashStyler, comparator: milesComparator, filter: "agNumberColumnFilter", width: 110},
-        {headerName: "First", field: "costs.first.miles", valueFormatter: milesAndCashFormatter, cellStyle: milesAndCashStyler, comparator: milesComparator, filter: "agNumberColumnFilter", width: 110}
-      ],
-      enableSorting: true,
-      enableFilter: true,
-
-      rowSelection: "single",
-      suppressCellSelection: false,
-      suppressRowClickSelection: true,
-
-      pagination: true,
-      paginationPageSize: PAGINATION_SIZE,
-
-      onFilterChanged: updateFilterValue,
-      onRowClicked: params => console.log(`Selected flight details: ${JSON.stringify(params.data, null, 2)}`)
-    }
-
-    new agGrid.Grid(gridDiv, gridOptions)   // eslint-disable-line no-new
-    this.grid = gridOptions
-    this.grid.api.setRowData([])
+  static onRowClicked(params) {
+    console.log(`Selected flight details: ${JSON.stringify(params.data, null, 2)}`)
   }
 }
