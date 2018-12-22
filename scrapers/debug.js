7c1e324612663ac3f9a01a4cbb2e378f04408e77
Larry Gadea <trivex@gmail.com>
2018-12-21 22:34:01 -0800

    initial commit of ita stuff -- needs to still parse results from web request

--- scrapers/debug.js
+++ scrapers/debug.js
@@ -1,19 +1,17 @@
 /* eslint-disable no-process-env */
 
 const index = require("./index")
-const process = require("process")
+//const process = require("process")
 
 const main = async() => {
   console.log("Starting")
   const result = await index.debugEntry({
-    scraper: "aeroplan",
+    scraper: "ita",
     params: {
-      from: "SFO",
-      to: "YOW",
-      date: "2018-12-01",
-      maxConnections: 1,
-      aeroplanUsername: process.env.AEROPLAN_USERNAME,
-      aeroplanPassword: process.env.AEROPLAN_PASSWORD
+      from: "LAX",
+      to: "JFK",
+      date: "2018-12-25",
+      maxConnections: 0
     }
   })
   console.log("Done")
