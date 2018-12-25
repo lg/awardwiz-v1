0f2e7df2f15f080e81da42a49637c427238b5cb9
Larry Gadea <trivex@gmail.com>
2018-12-24 16:50:26 -1000

    made ita selection of origin/destination much more reliable. plus added delay to prevent ita from detecting scraping. plus fixed year over-lap. plus fixed a 12-hr to 24-hr bug

--- scrapers/ita.js
+++ scrapers/ita.js
@@ -1,12 +1,18 @@
 /* eslint-disable no-await-in-loop */
 
+// ITA has multiple methods to mess with scraping such that search results will be arbitrarily
+// delayed and only give 10 direct flights. Things that trigger this protection:
+//   - Existance of navigator.webdriver
+//   - Having notifications disabled in the webbrowser
+//   - Spending a very short amount of time on the search screen
+
 /**
  * @param {import("puppeteer").Page} page
  * @param {SearchQuery} input
  */
 exports.scraperMain = async(page, input) => {
   console.log("Going to homepage...")
-  await page.goto("https://matrix.itasoftware.com", {waitUntil: "networkidle0"})
+  await page.goto("https://matrix.itasoftware.com")
 
   console.log("Setting one way...")
   const tabs = await page.$$(".gwt-TabBarItem .gwt-HTML")
@@ -16,10 +22,16 @@ exports.scraperMain = async(page, input) => {
   const fields = await page.$$(".gwt-SuggestBox")
   await fields[2].focus()
   await page.keyboard.type(input.origin)
+  const originXPath = `//span[contains(text(), '(${input.origin})')]`
+  await page.waitForXPath(originXPath, {timeout: 0})
+  await page.evaluate(`document.evaluate("${originXPath}", document).iterateNext().click()`)
 
   console.log("Setting destination...")
   await fields[3].focus()
   await page.keyboard.type(input.destination)
+  const destinationXPath = `//span[contains(text(), '(${input.destination})')]`
+  await page.waitForXPath(destinationXPath, {timeout: 0})
+  await page.evaluate(`document.evaluate("${destinationXPath}", document).iterateNext().click()`)
 
   console.log("Setting no connections...")
   const [stopsElement] = (await page.$x("//label[contains(text(), 'Stops')]/..//select"))
@@ -32,33 +44,38 @@ exports.scraperMain = async(page, input) => {
   await page.keyboard.type(`${input.date.substr(5, 2)}/${input.date.substr(8, 2)}/${input.date.substr(0, 4)}`)    // mm/dd/year
   await tabs[1].click()     // hide calendar
 
+  console.log("Waiting 5 seconds to make sure ITA doesnt flag us as a bot...")
+  await page.waitFor(5000)
+
   console.log("Starting search...")
   await Promise.all([
     page.click("button"),
-    page.waitForResponse("https://matrix.itasoftware.com/search"),
-    page.waitForResponse("https://matrix.itasoftware.com/pricecurve")
+    page.waitForResponse("https://matrix.itasoftware.com/search", {timeout: 0}),
+    page.waitForResponse("https://matrix.itasoftware.com/pricecurve", {timeout: 0})
   ])
 
-  // // If there are multiple pages, request everything
-  // const allLink = await page.$x("//a[text()='All']")
-  // if (allLink.length > 0) {
-  //   await Promise.all([
-  //     allLink[0].click(),
-  //     page.waitForXPath("//span[text()='All']")
-  //   ])
-  // }
+  // If there are multiple pages, request everything
+  const allLink = await page.$x("//a[text()='All']")
+  if (allLink.length > 0) {
+    console.log("Loading the 'All' page...")
+    await Promise.all([
+      allLink[0].click(),
+      page.waitForXPath("//span[text()='All']", {timeout: 0})
+    ])
+  }
 
   // Unfortunately the AJAX request is all messed up, so we'll need to scrape the UI
+  console.log("Parsing results UI...")
 
   /** Takes a 12-hour (1 or 2 digit hour) time, and converts it to 24-hour (2 digit hour)
    * @param {string} twelveHour
    * @returns {string} */
   const convert12HourTo24Hour = twelveHour => {
-    const [hour, minute] = twelveHour.split(":")
-
-    if (minute.toUpperCase().indexOf("A") >= 0)
-      return `${hour.padStart(2, "0")}:${minute.substr(0, 2)}`
-    return `${(parseInt(hour, 10) + 12).toString().padStart(2, "0")}:${minute.substr(0, 2)}`
+    const [rawHour, rawMinute] = twelveHour.split(":")
+    const [hour, minute] = [parseInt(rawHour, 10), parseInt(rawMinute.substr(0, 2), 10)]
+    if (twelveHour.toUpperCase().indexOf("AM") >= 0)
+      return `${(hour === 12 ? 0 : hour).toString().padStart(2, "0")}:${minute.toString().padStart(2, "0")}`
+    return `${(hour === 12 ? 12 : hour + 12).toString().padStart(2, "0")}:${minute.toString().padStart(2, "0")}`
   }
 
   /** Returns the innerText of an XPath query on a page/element
@@ -105,8 +122,16 @@ exports.scraperMain = async(page, input) => {
         const rawArrivalTimeWhenBoth = await xPathInnerText(".//table/*//td[3]/div[1]/div[1]", resultElement)
         const rawArrivalDateWhenBoth = await xPathInnerText(".//table/*//td[3]/div[1]/div[2]", resultElement)
 
-        // TODO: Year isn't necessarily this year (both for Dec->Jan and Jan->Dec)
-        arrivalDateTime = `${input.date.substr(0, 4)}-${(months.indexOf(month) + 1).toString().padStart(2, "0")}-${rawArrivalDateWhenBoth.substr(4)} ${convert12HourTo24Hour(rawArrivalTimeWhenBoth)}`
+        // Handle the year-change edge-case
+        const searchMonthStr = months[parseInt(input.date.substr(6, 2), 10) - 1]
+        let resultYear = parseInt(input.date.substr(0, 4), 10)  // Start it at the search year
+        if (searchMonthStr === "Dec" && month === "Jan") {
+          resultYear += 1
+        } else if (searchMonthStr === "Jan" && month === "Dec") {
+          resultYear -= 1
+        }
+
+        arrivalDateTime = `${resultYear.toString()}-${(months.indexOf(month) + 1).toString().padStart(2, "0")}-${rawArrivalDateWhenBoth.substr(4).padStart(2, "0")} ${convert12HourTo24Hour(rawArrivalTimeWhenBoth)}`
         break
       }
     }
@@ -117,5 +142,6 @@ exports.scraperMain = async(page, input) => {
     results.push(result)
   }
 
+  console.log("Done.")
   return {searchResults: results}
 }
