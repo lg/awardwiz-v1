3b3b5686445d26431a4fbd28a33cb3a0bb2f36b9
Larry Gadea <trivex@gmail.com>
2019-11-30 18:43:40 +0000

    made chrome installation way smaller plus put it back in autoinstall

--- .devcontainer/install-chrome-remote-desktop.sh
+++ .devcontainer/install-chrome-remote-desktop.sh
@@ -1,15 +1,14 @@
-# as per: https://cloud.google.com/solutions/chrome-desktop-remote-on-compute-engine
+# add the display environment variable for making debugging easier
+echo 'DISPLAY=:20.0' >> ~/workspace/.env
 
-# install chrome remote desktop
+# install chrome remote desktop as per: https://cloud.google.com/solutions/chrome-desktop-remote-on-compute-engine
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt update
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-
-# install the X server and a proper screensaver that's compatible with chrome remote desktop
-sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base
-sudo apt install --assume-yes xscreensaver
-
-sudo systemctl disable lightdm.service
-
 rm chrome-remote-desktop_current_amd64.deb
+
+# install the X server and disable screensaver
+sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+sudo systemctl disable lightdm.service
